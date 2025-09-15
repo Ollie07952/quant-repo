d313c1d83a6b577bb6b133383f559e3975221fe3
Ollie <O.Cole1-24@student.lboro.ac.uk>
2025-09-15 23:15:12 +0100

    Applied similar restructuring to vol.py such that it now accepts a str of tickers and a period for which to compute rolling and stationary annualised volatilities over

--- vol.py
+++ vol.py
@@ -1,18 +1,21 @@
-def vol(prices,k = 30):
+def vol(tickers,period,k = 30):
     """
-    Calculates daily and annualised volatility for each set of prices
+    Calculates rolling and stationary annualised volatility for each given ticker
     --
-    :arg prices: array; array(s) of prices
-    :arg k: int; the history of the rolling volatility, in days
+    :arg tickers: str; ticker list as a single str of the form "ABC DEFG XYZ ..."
+    :arg period: str; the period of returns to analyse; one of: [1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max]
+    :arg k: int; the history of the rolling volatility, in days. i.e. size of the rolling volatility window.
+                 NOTE: k MUST NOT BE GREATER IN LENGTH THAN CHOSEN PERIOD
     --
-    :returns roll_vol: array; rolling volatility with history k for each set of prices
-    :returns ann_vol: array; annualised volatility of each set of prices
+    :returns roll: array; rolling annualised volatility with history k for each ticker
+    :returns ann: array; annualised volatility of each ticker over the given period
     """
     import numpy as np
+    from logReturns import logReturns
 
-    if k > np.shape(prices)[1]:
-        raise ValueError("volatility history cannot be larger than number of days")
-    logs = np.atleast_2d([[np.log(x/price_sets[max(i-1,0)]) for i,x in enumerate(price_sets)] for price_sets in prices])
-    roll_vol = np.concatenate((np.full(shape = (np.shape(logs)[0],k-1),fill_value = np.nan),np.array([[np.std(log_sets[i-k:i], ddof = 1) for i in range(k, len(log_sets))] for log_sets in logs])),axis = 1)
-    ann_vol = np.array([np.std(log_sets, ddof = 1)*np.sqrt(252) for log_sets in logs])
-    return roll_vol, ann_vol
+    logs = logReturns(tickers, period)
+    roll = logs.rolling(window = k).std()*np.sqrt(252)
+    ann = logs.std(ddof = 1, axis = 0)*np.sqrt(252)
+    ann.name = "Annualised Vol"
+    ann.index.name = "Ticker"
+    return roll, ann
